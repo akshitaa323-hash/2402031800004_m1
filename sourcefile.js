c800a3e6fb72ef3612c53eb709d836f9738034d8
akshitaa323-hash <akshitaa323@gmail.com>
2026-05-14 14:19:03 +0530

    Javascript

---
+++ sourcefile.js
@@ -0,0 +1,10 @@
+document.write("<hr>");
+document.write("Hello Cutie!");
+document.write("<hr>");
+
+ var fruits = ["Apple"," ", "Orange"," ", "Mango"," ", "Banana"];
+
+for(var a in fruits)
+{
+    document.write(fruits[a]);
+}
