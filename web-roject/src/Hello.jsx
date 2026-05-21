1cdfa420638e32d0c1ee310e7db48e7fc105202e
akshitaa323-hash <akshitaa323@gmail.com>
2026-05-21 13:41:20 +0530

    pushed my portfolio

--- web-roject/src/Hello.jsx
+++ web-roject/src/Hello.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 function Hello() {
-  return (
-    <div>
-      <h1 className='test'>Akshita</h1>
-    </div>
-  )
-}
+  const isLoggedIn = true; 
 
+  if(isLoggedIn) {
+    return <h1>Welcome back, User!</h1>;
+  } else {
+    return <h1>Please log in to continue.</h1>;
+  }
+}
 export default Hello
